a35b471f2c5f43b20930fd4d1ff5d340dc7e1db4
Livid Su <qq1532250087@gmail.com>
2025-04-25 15:19:05 +0800

    redo

--- update_favicon.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -Path . -Filter "*.html" -Recurse | ForEach-Object {
-    $content = Get-Content $_.FullName -Raw
-    $content = $content -replace 'href="https://lividsu.com/favicon.ico"', 'href="favicon.ico"'
-    $content = $content -replace 'href="http://lividsu.com/favicon.ico"', 'href="favicon.ico"'
-    Set-Content $_.FullName $content
-} 
